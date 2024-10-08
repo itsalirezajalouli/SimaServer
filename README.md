f6f1563d3afc8b5baff66a91b234ff650286dd67
Alireza Jalouli <154112092+itsalirezajalouli@users.noreply.github.com>
2024-10-08 20:40:28 +0330

    Finwr

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Multi-Threaded Web Server in Rust
 
-I'm very passionate about Rust, and I thought what could be more fun than making a project with it.
+All hail to the Rust, and I thought what could be more fun than making a project with it.
 
-I always wanted to take a look at how servers work and dig deeper into backend development.
+Always wanted to take a look at how servers work and dig deeper into backend development.
 
-It's been a hell of a fun project, although I still struggle with many of the concepts in it.
+It's been a hell of a ride, although I still struggle with many of the concepts in it.
+
+Have fun using this code! NO LICENSE! NO LLM ALLOWED(F! U GPT)!
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender::<Job>,
+    sender: Option<mpsc::Sender::<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -33,7 +33,10 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool { 
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -46,12 +49,14 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -72,15 +77,24 @@ pub struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().expect("A worker got poisened!").recv().unwrap();
-            
-            println!("Worker {id} got a job; executing.");
+            let message = receiver.lock().unwrap().recv();
 
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
 
         Worker {
             id,
-            thread: Some(thread)} 
+            thread: Some(thread),
+        }
     }
 }
